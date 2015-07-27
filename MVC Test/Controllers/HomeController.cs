746cafea9471b9c410750899d728331101d2c31b
Arfen <allegro6@gazeta.pl>
2015-07-27 17:02:39 +0200

    done

---
+++ MVC Test/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using MVC_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var _entities = new CRMEntities();
+            var model = _entities.Klienci;
+          
+            return View(model);
+        }
+
+        public ActionResult EditCustomer(int? id)
+        {
+            return RedirectToAction("Edit", "KlienciEdit", new { id = id });
+        }
+
+        public ActionResult CreateCustomer(int? id)
+        {
+            return RedirectToAction("Create", "KlienciEdit", new { id = id });
+        }
+
+        public ActionResult DeleteCustomer(int? id)
+        {
+            return RedirectToAction("Delete", "KlienciEdit", new { id = id });
+        }
+
+        //[HttpPost]
+        //public ActionResult EditCustomer(Klienci kl)
+        //{
+        //    if (true)
+        //    {
+        //        this.EditCustomer(kl);
+        //        return RedirectToAction("Edit","KlienciEdit", kl.Id.ToString());
+        //    }
+        //    else
+        //    {
+        //        return PartialView(kl);
+        //    }
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
---
+++ MVC Test/Controllers/KlienciEditController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class KlienciEditController : Controller
+    {
+        private CRMEntities db = new CRMEntities();
+
+        // GET: KlienciEdit
+        public ActionResult Index(int id)
+        {
+            //var _entities = new CRMEntities();
+            //var model = _entities.Klienci.Where(m=>m.Id==id);
+            return View();
+        }
+
+        // GET: KlienciEdit/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Klienci klienci = db.Klienci.Find(id);
+            if (klienci == null)
+            {
+                return HttpNotFound();
+            }
+            return View(klienci);
+        }
+
+        // GET: KlienciEdit/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KlienciEdit/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nazwisko,Imie,Mail,Ulica,Miasto,Kod_pocztowy,TelefonKomorkowy,Friend")] Klienci klienci)
+        {
+            if (ModelState.IsValid)
+            {
+                Klienci kl = db.Klienci.OrderByDescending(x => x.Id).First();
+                klienci.Id = kl.Id + 1;
+                db.Klienci.Add(klienci);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(klienci);
+        }
+
+        // GET: KlienciEdit/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Klienci klienci = db.Klienci.Find(id);
+            if (klienci == null)
+            {
+                return HttpNotFound();
+            }
+            return View(klienci);
+        }
+
+        // POST: KlienciEdit/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Nazwisko,Imie,Mail,Ulica,Miasto,Kod_pocztowy,TelefonKomorkowy,Friend")] Klienci klienci)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(klienci).State = EntityState.Modified;
+                db.SaveChanges();
+                //return RedirectToAction("Index");  Comment mateusz
+                return RedirectToAction("Index", "Home");
+            }
+            return View(klienci);
+        }
+
+        // GET: KlienciEdit/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Klienci klienci = db.Klienci.Find(id);
+            if (klienci == null)
+            {
+                return HttpNotFound();
+            }
+            return View(klienci);
+        }
+
+        // POST: KlienciEdit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Klienci klienci = db.Klienci.Find(id);
+            db.Klienci.Remove(klienci);
+            db.SaveChanges();
+            //return RedirectToAction("Index");
+            return RedirectToAction("Index", "Home");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
